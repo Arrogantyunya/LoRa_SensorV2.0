aa1a12b8009bb4300a6c8547445048c8285b8841
刘家辉 <liujiahiu@qq.com>
2019-11-28 16:50:01 +0800

    添加了WS_100的初始代码

---
+++ 棚外雨量版_WS100/STM32_LORA_WS100_V1.0.0_alpha/STM32_LORA_WS100_V1.0.0_alpha.ino
@@ -0,0 +1,32 @@
+// Visual Micro is in vMicro>General>Tutorial Mode
+// 
+/*
+    Name:       STM32_LORA_WS100_V1.0.0_alpha.ino
+    Created:	2019/11/28 星期四 16:41:58
+    Author:     刘家辉
+*/
+
+// Define User Types below here or use a .h file
+//
+
+
+// Define Function Prototypes that use User Types below here or use a .h file
+//
+
+
+// Define Functions below here or use other .ino or cpp files
+//
+
+// The setup() function runs once each time the micro-controller starts
+void setup()
+{
+
+
+}
+
+// Add the main program code into the continuous loop() function
+void loop()
+{
+
+
+}
